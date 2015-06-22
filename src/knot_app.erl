6431634fcfa4ece37909e3ee45785758a8300e7d
Sebastian Green-Husted <ricecake@tfm.nu>
2015-06-21 20:08:01 -0400

    minor content changes

--- src/knot_app.erl
+++ src/knot_app.erl
@@ -14,11 +14,13 @@ start(_StartType, _StartArgs) ->
 		{ok, Pid} ->
 			Dispatch = cowboy_router:compile([
 				{'_', [
-					{"/", knot_page, index},
-					{"/js/[...]",  cowboy_static, {priv_dir, knot, "js/"}},
-					{"/css/[...]", cowboy_static, {priv_dir, knot, "css/"}},
-					{"/font/[...]", cowboy_static,{priv_dir, knot, "font/"}},
-					{"/img/[...]",  cowboy_static,{priv_dir, knot, "img/"}}
+					{"/",           knot_page,        index},
+					{"/ws/",        knot_msg_handler, #{}},
+					%Static file handlers
+					{"/js/[...]",   cowboy_static, {priv_dir, knot, "js/"}},
+					{"/css/[...]",  cowboy_static, {priv_dir, knot, "css/"}},
+					{"/font/[...]", cowboy_static, {priv_dir, knot, "font/"}},
+					{"/img/[...]",  cowboy_static, {priv_dir, knot, "img/"}}
 				]}
 			]),
 			{ok, _} = cowboy:start_http(http, 25, [{ip, {127,0,0,1}}, {port, 8080}],
--- src/knot_sup.erl
+++ src/knot_sup.erl
@@ -23,5 +23,8 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-	{ok, { {one_for_one, 5, 10}, []} }.
+	{ok, { {one_for_one, 5, 10}, [
+		?CHILD(knot_storage_srv, worker),
+		?CHILD(knot_session_sup, supervisor)
+	]} }.
 
