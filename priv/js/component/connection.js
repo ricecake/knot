66b2c0f9883f55f151e5903a3b56ef51f2d53a18
Sebastian Green-Husted <ricecake@tfm.nu>
2015-06-28 01:51:15 -0400

    start a class for managing server connection protocol

---
+++ priv/js/component/connection.js
@@ -0,0 +1,37 @@
+;(function(){
+'use strict';
+
+var defaults = {
+	eventHandlers: {}
+};
+
+var KnotConn = function (options) {
+	options = _.extend({}, defaults, options);
+	this.eventHandlers = options.eventHandlers;
+	var ws = this.WebSocket = new WebSocket(options.url);
+	ws.onopen = options.onOpen;
+	ws.onmessage = this._messageHandler.bind(this);
+};
+
+KnotConn.prototype._messageHandler = function(event) {
+	var type, content;
+	var decoded = JSON.parse(event.data);
+	type = decoded.type;
+	content = decoded.content;
+	console.log([type, content]);
+	_.map(this.eventHandlers[type], function(callback) {
+		callback(content);
+	});
+}
+
+KnotConn.prototype.send = function(key, content) {
+	var message = JSON.stringify({
+		'type': key,
+		'content': content
+	});
+	this.WebSocket.send(message);
+	return this;
+}
+
+window.KnotConn = KnotConn;
+}());
