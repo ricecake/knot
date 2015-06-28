c4665865e5cb817382acd86f9f65139e12355a79
Sebastian Green-Husted <ricecake@tfm.nu>
2015-06-28 02:41:51 -0400

    add some comments explaining intent of routing

--- priv/js/component/connection.js
+++ priv/js/component/connection.js
@@ -9,6 +9,16 @@
 //  return last.tree[curr];
 //},obj);
 
+/*
+ * Need to write some logic to implement trie behavior for matching
+ * Basically: '*' matches any one label; '#' matches any number of labels
+ * when a message comes in, we traverse the tree, executing callbacks in depth first pre order
+ * when we hit a star in the binding, we'll need to traverse the subtree with the next label popped off,
+ * regardless of what it is.
+ * When we hit a hash, we'll need to do that many times, untill we've exhausted the entire routing key,
+ * and then execute any callbacks bound to the hash itself.
+ */
+
 var defaults = {
 	eventHandlers: {}
 };
@@ -20,6 +30,15 @@ var KnotConn = function (options) {
 	var ws = this.WebSocket = new WebSocket(options.url);
 	ws.onopen = options.onOpen;
 	ws.onmessage = this._messageHandler.bind(this);
+};
+
+KnotConn.prototype.addEventHandlers = function(Handlers) {
+	_.each(Handlers, function(callback, key) {
+		key.split('.').reduce(function (node, label) {
+		  node.tree[label] = { value: [], tree: {} };
+		  return node.tree[label];
+		}, this.eventHandlers);
+	});
 };
 
 KnotConn.prototype._messageHandler = function(event) {
