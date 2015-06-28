67e8eb5600e9f2c39cc7f6dc8da94380596516c0
Sebastian Green-Husted <ricecake@tfm.nu>
2015-06-28 00:31:02 -0400

    start of fleshing out chat component

--- priv/js/component/chat.js
+++ priv/js/component/chat.js
@@ -1,4 +1,17 @@
-;(function(){
+;(function($){
 'use strict';
 
-}());
+var defaults = {
+	chatClass: 'knot-chat-window',
+	messageClass: 'knot-chat-message',
+	inputClass: 'knot-chat-input',
+	buttonClass: 'knot-chat-send-button'
+};
+
+$.fn.knotChat = function (options) {
+	options = $.extend({}, defaults, options);
+	return $(this).each(function() {
+	});
+}
+
+}(jQuery));
