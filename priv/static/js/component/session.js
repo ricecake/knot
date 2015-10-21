95027ad42141e1df9e3d39583a31589089a30220
Sebastian Green-Husted <ricecake@tfm.nu>
2015-10-20 20:38:48 -0400

    flesh out some of the session widget markup

--- priv/static/js/component/session.js
+++ priv/static/js/component/session.js
@@ -1,8 +1,20 @@
 ;(function($,_){
 'use strict';
-var containerMarkup = _.template("");
+var containerMarkup = _.template(
+"<div class='<%= sessionClass %>'>"+
+	"<ul class='<%= iconsClass %>'></ul>"+
+"</div>"
+);
+
+var iconMarkup = _.template(
+	"<li class='<%= iconClass %>'><span><%= identifier %></span></li>"
+);
 
 var defaults = {
+	sessionClass: 'knot-session-widgit',
+	iconsClass: 'knot-session-icon-container',
+	iconClass: 'knot-session-icon',
+	identifier: 'Anonymous User'
 };
 
 $.fn.knotSession = function (options) {
