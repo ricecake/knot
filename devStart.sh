e9e3b9727b524ca5a4cc54142af04c7f282dbe0d
Sebastian Green-Husted <ricecake@tfm.nu>
2015-11-02 20:57:37 -0500

    Remove deprecated start script

--- devStart.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-erl -pa `pwd`/deps/*/ebin -pa `pwd`/ebin -s knot -eval 'timer:apply_interval(5000, erlang, spawn, [fun()->[ code:load_file(Mod) || {Mod, _Path} <- code:all_loaded(), code:is_sticky(Mod) =/= true, code:soft_purge(Mod)] end]).'
