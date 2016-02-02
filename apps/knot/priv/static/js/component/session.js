6e0514f706d0f31ea97c4cbe974a3518b3e8f9ce
Sebastian Green-Husted <ricecake@tfm.nu>
2016-02-01 19:00:34 -0500

    prepare to start storing peer data

--- apps/knot/priv/static/js/component/session.js
+++ apps/knot/priv/static/js/component/session.js
@@ -1,4 +1,9 @@
-;define(['jquery', 'tpl!template/session','tpl!template/session-entry'], function($,containerMarkup,entry){
+;define([
+	'jquery',
+	'component/datastore',
+	'tpl!template/session',
+	'tpl!template/session-entry'
+], function($,KnotData,containerMarkup,entry){
 'use strict';
 var defaults = {
 	sessionClass: 'knot-session-widgit',
@@ -8,12 +13,14 @@ var defaults = {
 };
 
 $.fn.knotSession = function (options) {
+	var dataStore = new KnotData;
 	options = $.extend({}, defaults, options);
 	return $(this).each(function() {
 		var viewer = $(containerMarkup(options));
 		$(this).append(viewer);
 		options.connection.addEventHandlers({
-			'knot.session.join': function(key, content){
+			'knot.session.join': function(key, content, raw){
+				dataStore.set(raw.from, {});
 				viewer.find('.'+options.iconsClass).append($(entry($.extend({}, content, options))));
 			}
 		});
