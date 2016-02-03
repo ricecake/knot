1042652be46f456382ae868dcd3d0f932bfd1a32
Sebastian Green-Husted <ricecake@tfm.nu>
2016-02-02 20:32:18 -0500

    wip on monitoring for key changes

--- apps/knot/priv/static/js/component/datastore.js
+++ apps/knot/priv/static/js/component/datastore.js
@@ -2,11 +2,18 @@
 'use strict';
 
 var KnotData = function () {
+	this.monitors = {};
 	return this;
 };
 
 KnotData.prototype.set = function(key, value) {
-	return localStorage.setItem(key, JSON.stringify(value));
+	localStorage.setItem(key, JSON.stringify(value));
+	for (var watched in this.monitors) {
+		for (var i in this.monitors[watched]) {
+			this.do(key, this.monitors[watched][i]);
+		}
+	}
+	return value;
 };
 
 KnotData.prototype.get = function(key) {
@@ -18,6 +25,14 @@ KnotData.prototype.do = function(key, callback) {
 	var res = callback.call(value);
 	this.set(key, value);
 	return res;
+};
+
+KnotData.prototype.onChange = function(key, callback) {
+	if(!(key in this.monitors)) {
+		this.monitors[key] = [];
+	}
+	this.monitors[key].concat(callback);
+	return this;
 };
 
 return KnotData;
