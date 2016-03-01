750141c922a7d5e0209b08cbcdf2345007cad2e0
Sebastian Green-Husted <ricecake@tfm.nu>
2016-02-29 20:12:09 -0500

    very basic start of sharing file offers

--- apps/knot/priv/static/js/component/fileShare.js
+++ apps/knot/priv/static/js/component/fileShare.js
@@ -9,55 +9,56 @@ var conn;
 var defaults = {
 };
 
+var peerRouters = {};
+
+var sharedFilesLocal = {};
+var sharedFilesRemote = {};
+
 var pcm = new peerManager(function(session, Peer, initiator) {
+	var commonArgs = {
+		eventHandlers: {
+			'knot.fileshare.ping': function() {
+				this.state.ping = setTimeout(function(){
+					this.send('knot.fileshare.pong', null);
+				}.bind(this), 2500);
+			},
+			'knot.fileshare.pong': function() {
+				this.state.pong = setTimeout(function(){
+					this.send('knot.fileshare.ping', null);
+				}.bind(this), 2500);
+			},
+			'knot.fileshare.announce': function(key, content, raw) {
+				sharedFilesRemote[session] = sharedFilesRemote[session] || {};
+				sharedFilesRemote[session] = $.extend({}, sharedFilesRemote[session], content);
+				console.log(sharedFilesRemote);
+			}
+		},
+		onOpen: function(){
+			if(initiator) {
+				this.send('knot.fileshare.ping', null);
+			}
+			this.send('knot.fileshare.announce', sharedFilesLocal);
+		},
+		onClose: function(){
+			clearTimeout(this.state.ping);
+			clearTimeout(this.state.pong);
+			delete peerRouters[session];
+		}
+	};
+
 	if(initiator) {
-		var dc = new KnotConn({
+		peerRouters[session] = new KnotConn($.extend({}, commonArgs, {
 			connector: function(options) {
 				return Peer.createDataChannel("fileshare-negotiate");
 			},
-			onOpen: function(){
-				dc.send('knot.fileshare.ping', null);
-			},
-			eventHandlers: {
-				'knot.fileshare.ping': function() {
-					this.state.ping = setTimeout(function(){
-						dc.send('knot.fileshare.pong', null);
-					}, 2500);
-				},
-				'knot.fileshare.pong': function() {
-					this.state.pong = setTimeout(function(){
-						dc.send('knot.fileshare.ping', null);
-					}, 2500);
-				}
-			},
-			onClose: function(){
-				clearTimeout(this.state.ping);
-				clearTimeout(this.state.pong);
-			}
-		});
+		}));
 	}
 	Peer.ondatachannel = function(event) {
-		var dc = new KnotConn({
+		peerRouters[session] = new KnotConn($.extend({}, commonArgs, {
 			connector: function(options) {
 				return event.channel;
 			},
-			eventHandlers: {
-				'knot.fileshare.ping': function() {
-					this.state.ping = setTimeout(function(){
-						dc.send('knot.fileshare.pong', null);
-					}, 2500);
-				},
-				'knot.fileshare.pong': function() {
-					this.state.pong = setTimeout(function(){
-						dc.send('knot.fileshare.ping', null);
-					}, 2500);
-				}
-			},
-			onClose: function(){
-				clearTimeout(this.state.ping);
-				clearTimeout(this.state.pong);
-			}
-		});
+		}));
 	};
 });
 
@@ -68,6 +69,7 @@ $.fn.knotFileShare = function (options) {
 
 
 	return $(this).each(function() {
+		console.log(peerRouters);
 	});
 
 };
